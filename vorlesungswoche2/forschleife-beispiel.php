53a4e5d120d21bb4e449ab2e3f1b2014aa749dcb
Andreas Baumgart <andreas.baumgart@jade-hs.de>
2024-03-14 08:18:46 +0100

    update

--- vorlesungswoche2/forschleife-beispiel.php
+++ vorlesungswoche2/forschleife-beispiel.php
@@ -13,7 +13,10 @@
 		// Die Funktion rand(a,b) erzeugt eine Zufallszahl zwischen a und b. Siehe PHP-Dokumentation
 		for($schleifenzaehler=1;$schleifenzaehler<=10;$schleifenzaehler++)
 			{
-				echo "<tr><td>".rand(1,15)."</td><td>".rand(1,15)."</td><td>".rand(1,15)."</td></tr>";
+				if($schleifenzaehler!=4)
+				{
+					echo "<tr><td>Reihe $schleifenzaehler</td><td>".rand(1,15)."</td><td>".rand(1,15)."</td><td>".rand(1,15)."</td></tr>";
+				}
 			}
 	?>
 	</table>
