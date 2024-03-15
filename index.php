2f916b22e751ad124c5c7306bdf3e3e51c77a5a1
Andreas Baumgart <andreas.baumgart@jade-hs.de>
2024-03-15 12:06:55 +0100

    update

--- index.php
+++ index.php
@@ -7,9 +7,11 @@
 
 <body>
 	<!--- Kommentar für die Dokumentation des HTML Codes--->
-	<h1>Herzlich Willkommen!</h1>
-	<a href="vorlesungswoche1/">Vorlesungswoche 1</a>
-	<br>
-	<a href="vorlesungswoche2/">Vorlesungswoche 2</a>
+<h1>Herzlich Willkommen!</h1>
+<p>Die Verlinkungen auf die einzelnen Wochen sind nicht mehr aktiv. Der Grund ist, dass</p>
+<p>aufgrund der Einführung in JavaScript viele Dateien vorhanden sein werden. Die Videos in der Playlist haben</p>
+<p>im Videotitel den Dateinamen enthalten, auf die sich das Video bezieht. Ebenfalls sind vorerst alle PHP-Dateien entfernt, damit sie nicht so verwirren.</p>
+<p>Somit ist diese Seite vorerst nicht wichtig und wird nicht weiter gepflegt.<br>
+</p>
 </body>
 </html>
