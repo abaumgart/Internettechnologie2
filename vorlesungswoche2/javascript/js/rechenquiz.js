d99c1636ed756bc8821b80e36d217adcfbee522e
Andreas Baumgart <andreas.baumgart@jade-hs.de>
2024-03-21 12:11:43 +0100

    update

--- vorlesungswoche2/javascript/js/rechenquiz.js
+++ vorlesungswoche2/javascript/js/rechenquiz.js
@@ -2,8 +2,10 @@
 // Rechenquiz
 // Alle Befehle, die folgen, werden automatisch ausgeführt
 console.log('Seite geladen');
-let summand1=5;
-let summand2=10;
+let zahlenbereichMin=1;
+let zahlenbereichMax=10;
+let summand1=zufallszahl(zahlenbereichMin,zahlenbereichMax);
+let summand2=zufallszahl(zahlenbereichMin,zahlenbereichMax);
 let ergebnis = summand1+summand2;
 let aufgabentext=summand1+'+'+summand2;
 // Testweises Ausgeben der Aufgabe auf der Console
@@ -12,10 +14,23 @@ console.log(aufgabentext);
 //  Zugriff auf das Element mit der ID=aufgabe
 document.getElementById('aufgabe').value=aufgabentext;
 
+function zufallszahl(min, max)
+	{
+		return Math.floor(Math.random()*(max-min))+min;
+	}
+
 function check()
 {
 	console.log('Funktion wurde aufgerufen!');
 	// Wert auslesen, den der User eingetragen hat
 	let userWert= document.getElementById('userErgebnis').value;
 	console.log(userWert);
+	// Werte vergleichen
+	if(userWert==ergebnis)
+	   {
+	   		console.log('Richtig!');
+	   }else
+		   {
+			console.log('Falsch!');
+		   }
 }
